1f06a45a5862effbaafc8954c09d7c0aa0691c6d
Manuel Bieh <info@manuelbieh.de>
2018-10-24 00:52:11 +0200

    publications and trying to fix postcss-custom-media issues

--- src/pages/technologies.js
+++ src/pages/technologies.js
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react';
-import { Column, TechLogos } from '@manuel-bieh/ui/es';
+import { Column, Row, TechLogos } from '@manuel-bieh/ui/es';
 import { withIntl } from '../i18n';
 import { setContext } from '../components/PageContext';
 import PageWrapper from '../components/PageWrapper';
@@ -11,7 +11,7 @@ type PropsT = {
 
 const Technologies = ({ t, ...context }: PropsT) => (
     <PageWrapper title={t('technologies.title')} {...context}>
-        <>
+        <Row>
             <Column xs={12} lg={6}>
                 {t('technologies.intro').map((text) => (
                     <p key={text} dangerouslySetInnerHTML={{ __html: text }} />
@@ -20,7 +20,7 @@ const Technologies = ({ t, ...context }: PropsT) => (
             <Column xs={12} lg={6}>
                 <TechLogos />
             </Column>
-        </>
+        </Row>
     </PageWrapper>
 );
 
