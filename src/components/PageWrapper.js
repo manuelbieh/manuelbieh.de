29fcfaccdb4972ebd95159e39bc9ae8f8c9d2b17
Manuel Bieh <info@manuelbieh.de>
2018-11-12 01:42:40 +0100

    force ui package update

--- src/components/PageWrapper.js
+++ src/components/PageWrapper.js
@@ -2,7 +2,6 @@
 import * as React from 'react';
 import Helmet from 'react-helmet';
 import { Grid, Row, Column } from '@manuel-bieh/ui/es';
-import { PageContext } from '../components/PageContext';
 import Layout from './Layout';
 
 type PropsT = {
@@ -10,22 +9,18 @@ type PropsT = {
     title: string,
 };
 
-const PageWrapper = ({ children, title, ...props }: PropsT) => {
-    return (
-        <PageContext.Provider value={{ ...props }}>
-            <Layout>
-                {title && <Helmet title={title} />}
-                <Grid>
-                    <Row>
-                        <Column>
-                            <h1>{title}</h1>
-                        </Column>
-                    </Row>
-                    {children}
-                </Grid>
-            </Layout>
-        </PageContext.Provider>
-    );
-};
+const PageWrapper = ({ children, title }: PropsT) => (
+    <Layout>
+        {title && <Helmet title={title} />}
+        <Grid>
+            <Row>
+                <Column>
+                    <h1>{title}</h1>
+                </Column>
+            </Row>
+            {children}
+        </Grid>
+    </Layout>
+);
 
 export default PageWrapper;
