ba8c9c73b30a8ff8aaf9e6b79d6808d4c224c4c3
Manuel Bieh <github@manuelbieh.de>
2020-04-28 14:08:26 +0100

    chore: use devmode for build for better debugging

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -1,6 +1,6 @@
+import { Grid, Column } from '@manuel-bieh/ui/dist/es';
 import * as React from 'react';
 import Helmet from 'react-helmet';
-import { Grid, Column } from '@manuel-bieh/ui/dist/es';
 import { setContext } from '../components/PageContext';
 import Layout from './Layout';
 
